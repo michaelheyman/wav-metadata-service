9738f50c2f0a451cb64269ed7538910b8a3afe5d
Michael Heyman <michaelheyman@users.noreply.github.com>
2020-05-25 19:24:07 -0700

    Integrate simple example reading WAV files

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,40 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
+
 module Lib
     ( startApp
     , app
     ) where
 
+import           Control.Monad
+import           Control.Monad.IO.Class
 import           Data.Aeson
 import           Data.Aeson.TH
+import           Data.Text
+import qualified Data.Text.IO             as T
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
 import           Servant.Multipart
+import           Sound.Wav.Parser
+import           Sound.Wav.Parser.Types
+import           Stubs
 
 data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+    { userId        :: Int
+    , userFirstName :: String
+    , userLastName  :: String
+    } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''User)
 
-type API = "upload" :> MultipartForm Tmp (MultipartData Tmp) :> Post '[PlainText] String
+instance ToJSON Wav where
+    toJSON (Wav riffChunk fmtChunk dataChunk) =
+        object []
+
+type API = "upload" :> MultipartForm Tmp (MultipartData Tmp) :> Post '[JSON] [Wav]
 
 startApp :: IO ()
 startApp = do
@@ -39,9 +52,17 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server multipartData = return str
-    where str = "The form was submitted with "
-             ++ show nInputs ++ " textual inputs and "
-             ++ show nFiles  ++ " files."
-          nInputs = length (inputs multipartData)
-          nFiles  = length (files multipartData)
+server multipartData = liftIO $
+    forM (files multipartData) $ \file -> do
+        let wavFile = fdPayload file
+        result <- parseWavFile wavFile
+        case result of
+            Left e    -> return emptyWav
+            Right wav -> do
+                putStrLn $ "Retrieved wav file" ++ show wav
+                return wav
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
